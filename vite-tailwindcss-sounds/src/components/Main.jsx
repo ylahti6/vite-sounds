4540a87cc7c1d504991d9cc2131cb50f01300124
yannick <himynameisyannick@gmail.com>
2023-07-07 22:09:41 +0100

    changes
    changes made to the background video

--- vite-tailwindcss-sounds/src/components/Main.jsx
+++ vite-tailwindcss-sounds/src/components/Main.jsx
@@ -1,23 +1,28 @@
 import React, { useRef, useState } from "react";
 import audioFile from "../audio/vite-tailwindcss-sound.mp3";
-import rainBackground from "../img/vite-tailwindcss-rain-background.jpg";
-import { AiOutlinePlayCircle, AiOutlinePauseCircle, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import rainBackground from "../video/vite-tailwindcss-video.mp4";
+import { AiOutlinePlayCircle, AiOutlinePauseCircle } from "react-icons/ai";
 
 const Main = () => {
   const audioRef = useRef(null);
+  const videoRef = useRef(null);
   const playButtonRef = useRef(null);
   const pauseButtonRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [videoSpeed, setVideoSpeed] = useState(0.5);
 
   const playPause = () => {
     const audio = audioRef.current;
+    const video = videoRef.current;
 
     if (audio.paused) {
       audio.play();
       setIsPlaying(true);
+      video.playbackRate = 1; // Reset video speed to normal when playing
     } else {
       audio.pause();
       setIsPlaying(false);
+      video.playbackRate = 0.5; // Set video speed to 0.5x when paused
     }
   };
 
@@ -27,71 +32,73 @@ const Main = () => {
   };
 
   return (
-    <div
-      className="bg-cover bg-center h-screen w-screen capitalize"
-      style={{ backgroundImage: `url(${rainBackground})` }}
-    >
-      <div className="grid justify-center items-center h-screen">
-      <div className="flex flex-col items-center justify-center rounded-3xl h-fit w-fit p-4 font-serif bg-[#fffff33] backdrop-blur-md shadow-2xl drop-shadow-lg">
-        {/* Audio file */}
-        <audio loop ref={audioRef} id="song" className="hidden">
-          <source src={audioFile} type="audio/mpeg" />
-          <source src={audioFile} type="audio/mp3" />
-          <source src={audioFile} type="audio/ogg" />
-          Your browser does not support the audio element.
-        </audio>
+    <div className="relative h-screen w-screen">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src={rainBackground}
+        ref={videoRef}
+        autoPlay
+        loop
+        muted
+        playbackRate={videoSpeed}
+      ></video>
 
-        <div className="controls flex items-center justify-center m-8">
-          <div
-            id="playButton"
-            ref={playButtonRef}
-            style={{ display: isPlaying ? "none" : "block" }}
-            
-          >
-            <AiOutlinePlayCircle
-              size={150}
-              onClick={playPause}
-              className="text-white cursor-pointer hover:animate-pulse"
+      <div className="grid justify-center items-center h-screen font-serif">
+        <div className="flex flex-col items-center justify-cente space-y-2 space-y-reverse rounded-3xl h-fit w-fit bg-[#fffff33] backdrop-blur-xl shadow-2xl drop-shadow-xl">
+          <audio loop ref={audioRef} id="song" className="hidden">
+            <source src={audioFile} type="audio/mpeg" />
+            <source src={audioFile} type="audio/mp3" />
+            <source src={audioFile} type="audio/ogg" />
+            Your browser does not support the audio element.
+          </audio>
+
+          <div className="controls flex items-center justify-center p-8">
+            <div
+              id="playButton"
+              ref={playButtonRef}
+              style={{ display: isPlaying ? "none" : "block" }}
+            >
+              <AiOutlinePlayCircle
+                size={150}
+                onClick={playPause}
+                className="text-white cursor-pointer hover:animate-pulse"
+              />
+            </div>
+            <div
+              id="pauseButton"
+              ref={pauseButtonRef}
+              style={{ display: isPlaying ? "block" : "none" }}
+            >
+              <AiOutlinePauseCircle
+                size={150}
+                onClick={playPause}
+                className="text-white cursor-pointer hover:animate-pulse"
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center">
+            <input
+              type="range"
+              className="w-auto"
+              min="0"
+              max="1"
+              step="0.01"
+              defaultValue="0.5"
+              onChange={handleVolumeChange}
             />
           </div>
-          <div
-            id="pauseButton"
-            ref={pauseButtonRef}
-            style={{ display: isPlaying ? "block" : "none" }}
-          >
-            <AiOutlinePauseCircle
-              size={150}
-              onClick={playPause}
-              className="text-white  cursor-pointer hover:animate-pulse"
-            />
-          </div>
-        </div>
 
-        <div className="flex justify-center items-center">
-          <input
-            type="range"
-            className="w-auto"
-            min="0"
-            max="1"
-            step="0.01"
-            defaultValue="0.5"
-            onChange={handleVolumeChange}
-          />
-        </div>
+          <h1 className="text-white text-4xl drop-shadow-md p-8 md:text-6xl">Rainy Mood</h1>
 
-        <h1 className="text-white text-4xl m-8 drop-shadow-md">Rainy Mood.</h1>
-
-        <div className="flex">
-          <AiFillGithub size={50} className="text-white m-2"/>
-          <AiFillLinkedin size={50} className="text-white m-2"/>
+          <p className="text-white text-lg p-2 drop-shadow-md ">rain sounds for sleep & study</p>
+          <p className="text-white text-sm drop-shadow-md sm:text-md p-6">
+            Soon available on <span className="hover:underline cursor-pointer">Spotify</span> and{" "}
+            <span className="hover:underline cursor-pointer">Apple Music</span>*
+          </p>
         </div>
-        
-        <p className="text-white text-xl m-4 drop-shadow-md">rain sounds for sleep & study</p>
-        <p className="text-white text-sm m-4 drop-shadow-md">Soon available on <span className="hover:underline cursor-pointer">Spotify</span> and <span className="hover:underline cursor-pointer">Apple Music</span>*</p>
-       </div>
       </div>
-      </div>
-    
+    </div>
   );
 };
 
